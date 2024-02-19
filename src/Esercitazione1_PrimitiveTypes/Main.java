3d4f7326b64137566ca330fa718c058a3ffc0bf0
Matteo Solinas <matteo.solinas91@gmail.com>
2024-02-19 17:55:45 +0100

    add Esercitazione1_PrimitiveTypes
    aggiunto Esercitazione1_PrimitiveTypes

---
+++ src/Esercitazione1_PrimitiveTypes/Main.java
@@ -0,0 +1,14 @@
+package Esercitazione1_PrimitiveTypes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int base = 10;
+        int altezza = 15;
+        int area = base * altezza;
+
+        System.out.println(area);
+
+    }
+}
--- src/Esercizio1/Main.java
+++
@@ -1,8 +0,0 @@
-package Esercizio1;
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }
-}
