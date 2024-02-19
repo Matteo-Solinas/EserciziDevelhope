ac23ce308479f0deef4b8e043cf1267a5cc398fe
Matteo Solinas <matteo.solinas91@gmail.com>
2024-02-19 18:04:32 +0100

    aggiunto Esercitazione2
    aggiunto Esercitazione2

---
+++ src/Esercitazione2_PrimitiveTypes/Main.java
@@ -0,0 +1,14 @@
+package Esercitazione2_PrimitiveTypes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char a = 'a';
+        char b = 'b';
+        String combined = "" + a + b ;
+
+        System.out.println(combined);
+
+    }
+}
