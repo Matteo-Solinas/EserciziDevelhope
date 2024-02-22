9483ab7504c2479bdb29b0880ce2baf52425d5cf
Matteo Solinas <matteo.solinas91@gmail.com>
2024-02-22 19:24:44 +0100

    correct typo in Main.java

--- src/Esercitazione24_Array1/Main.java
+++ src/Esercitazione24_Array1/Main.java
@@ -3,7 +3,7 @@ package Esercitazione24_Array1;
 public class Main {
 
     /* Scrivi un programma che contenga un metodo che crea un array e lo riempie
-     * con i numeri da 1 a 10 e che che calcoli la somma dei numeri contenuti nell'array
+     * con i numeri da 1 a 10 e che calcoli la somma dei numeri contenuti nell'array
      * inizializzato e la stampi a video.*/
 
 
