5a311388dad5978d5fce72c4627c7ea004307845
Matteo Solinas <matteo.solinas91@gmail.com>
2024-02-26 23:06:19 +0100

    Update Main.java
    aggiunta traccia esercizio

--- src/Esercitazione28_ArrayMultidimensionali2/Main.java
+++ src/Esercitazione28_ArrayMultidimensionali2/Main.java
@@ -1,7 +1,18 @@
 package Esercitazione28_ArrayMultidimensionali2;
 
 public class Main {
+/*Scrivere un programma che contenga un metodo che permette di inizializzare una matrice e di scambiare
+  le sue righe e le sue colonne stampandola a video.
+Matrice di partenza:
 
+1 2 3
+4 5 6
+Matrice risultato
+
+ 1 4
+ 2 5
+ 3 6 */
+    
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
         System.out.println("matrice stampata normalmente");
