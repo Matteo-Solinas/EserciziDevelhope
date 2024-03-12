7a93342641e0d7c00bb0fca948f75737c3d16c13
Matteo Solinas <matteo.solinas91@gmail.com>
2024-03-12 18:13:23 +0100

    Update Main.java

--- JavaAdvanced/src/esercizio05_Nullability/Main.java
+++ JavaAdvanced/src/esercizio05_Nullability/Main.java
@@ -1,9 +1,11 @@
 package esercizio05_Nullability;
 
 public class Main {
+
     /*Scrivere una funzione che accetti in input un numeratore e un denominatore. Controlli che il numeratore
      e il denominatore non siano null. Usare la funzione appena scritta in due blocchi di codice dove
       si cattura l'eventuale eccezione.*/
+
     public static void main(String[] args) {
 
         Double numerator = 2.3;
