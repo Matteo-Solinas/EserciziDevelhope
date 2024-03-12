f75070f092c8c185d28697b987ca80727f20b724
Matteo Solinas <matteo.solinas91@gmail.com>
2024-03-12 18:12:24 +0100

    Update Main.java

--- JavaAdvanced/src/esercizio03_ExceptionsAndErrors03/Main.java
+++ JavaAdvanced/src/esercizio03_ExceptionsAndErrors03/Main.java
@@ -1,13 +1,15 @@
 package esercizio03_ExceptionsAndErrors03;
 
 public class Main {
+
     /*Scrivere una funzione che provi a dividere un numero per 0 e catturi l'eccezione leggendone il messaggio.*/
+
     public static void main(String[] args) {
 
-        System.out.println(checkDivisionbyZero(5));
+        System.out.println(checkDivisionByZero(5));
     }
 
-    public static int checkDivisionbyZero(int inputNum) {
+    public static int checkDivisionByZero(int inputNum) {
         try {
             return inputNum / 0;
         } catch (ArithmeticException e) {
