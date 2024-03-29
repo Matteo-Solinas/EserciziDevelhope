bb8fe960de93a3c2dd7fafdd6018f5a3390f18c7
Matteo Solinas <matteo.solinas91@gmail.com>
2024-03-29 10:30:25 +0100

    Fix comment Date05, Update Date04
    fix commento Date05, aggiunta localizzazione data a Date04

--- JavaAdvanced/src/esercizio19_Date04/Main.java
+++ JavaAdvanced/src/esercizio19_Date04/Main.java
@@ -13,28 +13,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-
-        System.out.println("date: " + date);
+        OffsetDateTime startingDateString = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        DateTimeFormatter startingDateStringFormat = DateTimeFormatter.ofPattern("dd/MMMM/yyyy").withLocale(Locale.ITALY)
+                .withZone(ZoneId.of("Europe/Rome"));
+        String startingDateStringLocalizedIta = startingDateString.format(startingDateStringFormat);
+        System.out.println("startingDate: " + startingDateStringLocalizedIta);
 
         //aggiunge un anno
-        date = date.plusYears(1);
-        System.out.println("datePlus1Year: " + date);
+        startingDateString = startingDateString.plusYears(1);
+        DateTimeFormatter dateFormatPlus1Year = DateTimeFormatter.ofPattern("dd/MMMM/yyyy").withLocale(Locale.ITALY)
+                .withZone(ZoneId.of("Europe/Rome"));
+        String dateLocalizedItaPlus1Year = startingDateString.format(dateFormatPlus1Year);
+        System.out.println("datePlus1Year: " + dateLocalizedItaPlus1Year);
 
         //sottrae un mese
-        date = date.minusMonths(1);
-        System.out.println("dateMinus1Month: " + date);
+        startingDateString = startingDateString.minusMonths(1);
+        DateTimeFormatter dateFormatMinus1Month = DateTimeFormatter.ofPattern("dd/MMMM/yyyy").withLocale(Locale.ITALY)
+                .withZone(ZoneId.of("Europe/Rome"));
+        String dateLocalizedItaMinus1Month = startingDateString.format(dateFormatMinus1Month);
+        System.out.println("dateMinus1Month: " + dateLocalizedItaMinus1Month);
 
         //aggiunge una settimana
-        date = date.plusWeeks(1);
-        System.out.println("datePlus7Days: " + date);
-
-        //localizzazione formato e data italiana
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MMMM/yyyy HH:mm:ss").withLocale(Locale.ITALY)
+        startingDateString = startingDateString.plusWeeks(1);
+        DateTimeFormatter dateFormatPlus1Week = DateTimeFormatter.ofPattern("dd/MMMM/yyyy").withLocale(Locale.ITALY)
                 .withZone(ZoneId.of("Europe/Rome"));
+        String dateLocalizedIta1Week = startingDateString.format(dateFormatPlus1Week);
+        System.out.println("datePlus7Days: " + dateLocalizedIta1Week);
 
-        String dateLocalizedIta = date.format(dateFormat);
-        System.out.println("Data e ora localizzate per l'Italia: " + dateLocalizedIta);
+        //data aggiornata
+        DateTimeFormatter dateStringFormat = DateTimeFormatter.ofPattern("dd/MMMM/yyyy").withLocale(Locale.ITALY)
+                .withZone(ZoneId.of("Europe/Rome"));
+        String dateStringLocalizedIta = startingDateString.format(dateStringFormat);
+        System.out.println("Data aggiornata: " + dateStringLocalizedIta);
 
     }
 }
--- JavaAdvanced/src/esercizio20_Date05/Main.java
+++ JavaAdvanced/src/esercizio20_Date05/Main.java
@@ -8,7 +8,7 @@ public class Main {
        Crea un secondo oggetto data da questa stringa 2024-03-01T13:00:00Z;
        Verificare che la prima data è precedente alla seconda;
        Verificare che la seconda data è successiva alla prima;
-       Verificare che le due date sono uguali ad ora;(??)
+       Verificare che le due date sono uguali;
        Stampa il risultato; */
 
     public static void main(String[] args) {
