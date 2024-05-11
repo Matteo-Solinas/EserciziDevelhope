97aa1930b76c10937f4d3bf18b2f9f06e90bd27a
Matteo Solinas <matteo.solinas91@gmail.com>
2024-05-11 16:18:14 +0200

    Update NameController.java
    aggiunte annotazioni

--- Spring Boot/controllerSwaggerDoc/src/main/java/com/exercise/controllerSwaggerDoc/controllers/NameController.java
+++ Spring Boot/controllerSwaggerDoc/src/main/java/com/exercise/controllerSwaggerDoc/controllers/NameController.java
@@ -1,8 +1,14 @@
 package com.exercise.controllerSwaggerDoc.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Controller with Swagger")
 @RequestMapping("/user")
 public class NameController {
 
@@ -12,16 +18,32 @@ public class NameController {
     http://localhost:8080/v3/api-docs
     */
 
+    @Operation(summary = "getName", description = "Restituisce il nome inserito come input")
     @GetMapping(path = "/name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il nome viene restituito con successo"),
+            @ApiResponse(responseCode = "404", description = "La risorsa che si stava cercando di raggiungere non è stata trovata")
+    })
     public String getName(
-            @RequestParam String nome
+            @RequestParam(required = true, name = "nome")
+            @Parameter(name = "nome",
+                    description = "Nome inserito",
+                    example = "Mario") String nome
     ) {
         return "Il nome è " + nome;
     }
 
+    @Operation(summary = "postReverseName", description = "Il nome inserito come input viene restituito invertito")
     @PostMapping(path = "/reverseName")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il nome viene invertito con successo"),
+            @ApiResponse(responseCode = "404", description = "La risorsa che si stava cercando di raggiungere non è stata trovata")
+    })
     public String postReverseName(
-            @RequestParam String nome
+            @RequestParam(required = true, name = "nome")
+            @Parameter(name = "nome",
+                    description = "Nome inserito",
+                    example = "Mario") String nome
     ) {
         return "Il nome " + nome + " invertito è: " + new StringBuilder(nome).reverse();
     }
