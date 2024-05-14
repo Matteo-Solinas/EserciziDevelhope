3a101d722848839bf52aa2501a4b4632636ea7b7
Matteo Solinas <matteo.solinas91@gmail.com>
2024-05-14 16:07:53 +0200

    add ORM
    aggiunto esercizio di test ORM

---
+++ Spring Boot/ORM/src/test/java/com/exercise/ORM/OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
